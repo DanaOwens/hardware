c3a3549c92d425bf1e3dea34c6e5113a803be324
Ronin Sharma <rrs234@cornell.edu>
2020-02-29 16:45:48 -0500

    Added files for VN-100 severe movement detection

---
+++ VN-100/VN100_detectProblems.py
@@ -0,0 +1,25 @@
+!/usr/bin/env python
+
+"""
+Simple function to determine if there are major movements with the IMU based on the yaw, pitch, and roll.
+"""
+
+import pandas as pd
+
+def detectProblems():
+    # Read the csv
+    path = "VNYMR.csv"
+    df = pd.read_csv(path)
+    # Determine if there are any major changes in yaw, pitch, and roll
+    list = ["Yaw", "Pitch", "Roll"]
+    for item in list:
+        stats = df[item].describe()
+        mean = stats[1]
+        std = stats[2]
+        for val in df_pa[item]:
+            out_thres_max = mean + 1.5 * std # outlier threshold --- use this for now
+            out_thres_min = mean - 1.5 * std
+            # Profit
+            if val > out_thres_max or val < out_thres_min:
+                return True # returns true if there's a problem
+    return False
