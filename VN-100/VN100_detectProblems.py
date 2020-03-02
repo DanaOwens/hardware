12790a4ba9950187213322ba26316c982afaca90
Ronin Sharma <rrs234@cornell.edu>
2020-03-01 20:00:37 -0500

    Fixed minor issue

--- VN-100/VN100_detectProblems.py
+++ VN-100/VN100_detectProblems.py
@@ -10,15 +10,15 @@ def detectProblems():
     # Read the csv
     path = "VNYMR.csv"
     df = pd.read_csv(path)
-    # Determine if there are any major changes in yaw, pitch, and roll
+    # Determine if there are any major changes in yaw, pitch, or roll
     list = ["Yaw", "Pitch", "Roll"]
     for item in list:
-        stats = df[item].describe()
+        stats = df[item].describe() # Get summary statistics
         mean = stats[1]
         std = stats[2]
-        for val in df_pa[item]:
-            out_thres_max = mean + 1.5 * std # outlier threshold --- use this for now
-            out_thres_min = mean - 1.5 * std
+        out_thres_max = mean + 1.5 * std # outlier threshold --- use this for now
+        out_thres_min = mean - 1.5 * std
+        for val in df[item]:
             # Profit
             if val > out_thres_max or val < out_thres_min:
                 return True # returns true if there's a problem
