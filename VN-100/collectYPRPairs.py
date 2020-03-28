2c7df6b0057de1123248eac0addbded67e96838a
Ronin Sharma <rrs234@cornell.edu>
2020-03-28 19:05:58 -0400

    Updated to keep reading pairwise values until prompted to stop

--- VN-100/collectYPRPairs.py
+++ VN-100/collectYPRPairs.py
@@ -7,6 +7,7 @@ and if there were major movements.
 
 from vnpy import *
 
+
 def main():
     # Setup sensor
     s = VnSensor()
@@ -14,20 +15,24 @@ def main():
     file = "test.txt" # filename
 
     # Setup IMU for the first reading then press enter
-    x = input("Prepare for first reading")
-    ypr1 = [s.read_yaw_pitch_roll().x, s.read_yaw_pitch_roll().y, s.read_yaw_pitch_roll().z]
+    x = input("Prepare for first reading (enter 'stop' to stop)")
+    while ("stop" not in x.lower()):
+        
+        ypr1 = [s.read_yaw_pitch_roll().x, s.read_yaw_pitch_roll().y, s.read_yaw_pitch_roll().z]
 
-    # Setup IMU for the second reading then press enter
-    y = input("Prepare for second reading")
-    ypr2 = [s.read_yaw_pitch_roll().x, s.read_yaw_pitch_roll().y, s.read_yaw_pitch_roll().z]
+        # Setup IMU for the second reading then press enter
+        y = input("Prepare for second reading")
+        ypr2 = [s.read_yaw_pitch_roll().x, s.read_yaw_pitch_roll().y, s.read_yaw_pitch_roll().z]
 
-    z = input("Enter 0 if there was major movement, otherwise enter 1: ")
-    while (z not in ["0", "1"]):
         z = input("Enter 0 if there was major movement, otherwise enter 1: ")
+        while (z not in ["0", "1"]):
+            z = input("Enter 0 if there was major movement, otherwise enter 1: ")
 
-    f = open(file, "a+")
-    f.write(str(ypr1) + " " + str(ypr2) + " " + z + "\n")
-    f.close()
+        f = open(file, "a+")
+        f.write(str(ypr1) + " " + str(ypr2) + " " + z + "\n")
+        f.close()
+        x = input("Prepare for first reading (enter stop to stop)")
+
 
 if __name__ == "__main__":
     main()
