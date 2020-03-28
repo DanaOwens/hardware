727b80420de57387ddaff108f3230706f1995830
Ronin Sharma <rrs234@cornell.edu>
2020-03-28 19:04:47 -0400

    Updated with working 2nd algorithm

--- VN-100/main.py
+++ VN-100/main.py
@@ -6,44 +6,17 @@ based on the yaw, pitch, and roll.
 """
 
 from vnpy import *
-
-# Initialize values
-count = 0
-prevYaw = 0
-prevPitch = 0
-prevRoll = 0
-yawAvg = 0
-pitchAvg = 0
-rollAvg = 0
-s = 0
+import time
 
 
-def checkYPR():
-    currYPR = s.read_yaw_pitch_roll()
-    yaw = currYPR.x
-    pitch = currYPR.y
-    roll = currYPR.z
-    
-    # Update average values
-    count += 1
-    yawAvg = ((yawAvg * (count - 1)) + yaw) / count
-    pitchAvg = ((pitchAvg * (count - 1)) + pitch) / count
-    rollAvg = ((rollAvg * (count - 1)) + roll) / count
-
-    # Base algorithm: extreme movement if any of YPR is larger than average
-    # Next algorithm: Compare the previous and current YPR values to assess major movement
+def checkYPR(s, prevYaw, prevPitch, prevRoll):
+    # Algorithm: Compare the previous and current YPR values to assess major movement
         # Problem:
-				# Roll or pitch changes by 45 degrees or more
-				# Yaw changes by 60 degrees or more
-    # return yaw > 1.5 * yawAvg or pitch > 1.5 * pitchAvg or roll > 1.5 * rollAvg
+			# Roll or pitch changes by 45 degrees or more
+		    # Yaw changes by 60 degrees or more
+    currYPR = s.read_yaw_pitch_roll()
 
-    problem = abs(roll - prevRoll) >= 45 or abs(pitch - prevPitch) >= 45  or abs(yaw - prevYaw) >= 60
-
-    # Update previous values
-    prevYaw = yaw
-    prevPitch = pitch
-    prevRoll = roll
-
+    problem = abs(currYPR.z - prevRoll) >= 45 or abs(currYPR.y - prevPitch) >= 45  or abs(currYPR.x - prevYaw) >= 60
     return problem
 
 
@@ -51,14 +24,17 @@ def main():
     # Setup sensor
     s = VnSensor()
     s.connect("COM5", 115200) # change the connection parameter appropriately
-    prevYaw = s.read_yaw_pitch_roll().x
-    prevPitch = s.read_yaw_pitch_roll().y
-    prevRoll = s.read_yaw_pitch_roll().z
 
     while(True):
-        problem = checkYPR()
+        currYPR = s.read_yaw_pitch_roll()
+        yaw = currYPR.x
+        pitch = currYPR.y
+        roll = currYPR.z
+        time.sleep(0.5) # Delay 0.5 seconds
+        problem = checkYPR(s, yaw, pitch, roll)
+
         if (problem):
-            # Do something
+        # Do something
             print("Problem")
         else:
             print("No problem")
@@ -66,4 +42,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
