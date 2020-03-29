85034250939737717df205c0dabb7640674aaeb3
Ronin Sharma <rrs234@cornell.edu>
2020-03-29 18:07:48 -0400

    Modified pairwise script output format

--- VN-100/collectYPRPairs.py
+++ VN-100/collectYPRPairs.py
@@ -18,19 +18,21 @@ def main():
     x = input("Prepare for first reading (enter 'stop' to stop)")
     while ("stop" not in x.lower()):
         
-        ypr1 = [s.read_yaw_pitch_roll().x, s.read_yaw_pitch_roll().y, s.read_yaw_pitch_roll().z]
+        out1 = str(s.read_yaw_pitch_roll().x) + ", " + str(s.read_yaw_pitch_roll().y) + ", " + str(s.read_yaw_pitch_roll().z)
 
         # Setup IMU for the second reading then press enter
         y = input("Prepare for second reading")
-        ypr2 = [s.read_yaw_pitch_roll().x, s.read_yaw_pitch_roll().y, s.read_yaw_pitch_roll().z]
+        out2 = str(s.read_yaw_pitch_roll().x) + ", " + str(s.read_yaw_pitch_roll().y) + ", " + str(s.read_yaw_pitch_roll().z)
 
         z = input("Enter 0 if there was major movement, otherwise enter 1: ")
         while (z not in ["0", "1"]):
             z = input("Enter 0 if there was major movement, otherwise enter 1: ")
 
         f = open(file, "a+")
-        f.write(str(ypr1) + " " + str(ypr2) + " " + z + "\n")
+        output = out1 + ", " + out2 + ", " + z
+        f.write(output + "\n")
         f.close()
+
         x = input("Prepare for first reading (enter stop to stop)")
 
 
